c336e8d28b5d33bd54a3545f2aaf23a10253cfdd
Urva <patelurva2806@gmail.com>
2023-07-31 19:30:20 +0530

    Javascript Fundamentals

---
+++ mathfunction.js
@@ -0,0 +1,13 @@
+let num = 10.6;
+console.log(Math.floor(num)); //10
+console.log(Math.ceil(num)); //11
+console.log(Math.round(num)); //11
+
+let num1 = 12.36;
+console.log(num1.toFixed(1));
+
+console.log(Math.max(3, 5, -10, 0, 1)); // 5
+
+console.log(Math.pow(2, 10)); // 2 in power 10 = 1024
+
+console.log(Math.random()); // print any random  number between 0 and 1
