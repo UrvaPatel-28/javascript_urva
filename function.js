2532bc3fe8ebf8f4dbaa9ac0f1b8223c736ebbee
Urva <patelurva2806@gmail.com>
2023-08-02 19:09:07 +0530

    javascript fundamentals

---
+++ function.js
@@ -0,0 +1,13 @@
+
+function myFunction(a, b) {
+
+    return a * b;
+}
+let x = myFunction(4, 3);
+console.log(x)
+
+//arrow function
+const a = (a, b) => {
+    return a * b;
+}
+console.log(a(22, 3))
---
+++ string_methods.js
@@ -0,0 +1,52 @@
+//length()
+// let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let x = text.length;
+// console.log(x)
+
+//slice()
+// let text = "AppleBananaKiwi";
+// let x = text.slice(5, 11);
+// console.log(x)
+
+//substring()
+// let text = "AppleBananaKiwi";
+// let x = text.substring(5, 11);
+// console.log(x)
+
+//replace()
+// let text = "hello urva";
+// let x = text.replace('hello', 'hii');
+// console.log(x)
+
+//toUpperCase()
+// let text = "hello"
+// let x = text.toUpperCase()
+// console.log(x)
+
+//toLowerCase()
+// let text = "HELLO"
+// let x = text.toLowerCase()
+// console.log(x)
+
+//concat()
+// let text1 = "Hello";
+// let text2 = "World";
+// let x = text1.concat(" ", text2);
+// console.log(x)
+
+//trim()
+// let text1 = "      Hello World!      ";
+// let x = text1.trim();
+// console.log(x)
+
+
+//charAt()
+// let text = "HELLO WORLD";
+// let x = text.charAt(0);
+// console.log(x)
+
+//split()
+// let text = "a,b,c,d";
+// let x = text.split(",");
+// console.log(typeof x);
+// console.log(x);
